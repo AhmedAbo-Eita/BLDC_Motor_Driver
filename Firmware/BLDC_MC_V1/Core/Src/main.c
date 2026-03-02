a367bc61cb96b37b3341eeaed5b45104bd4e505c
Ahmed Aboeita <ahmedmasre2233@gmail.com>
2026-03-02 23:48:10 +0200

    Edit the project with the pins selected in HW

--- Firmware/BLDC_MC_V1/Core/Src/main.c
+++ Firmware/BLDC_MC_V1/Core/Src/main.c
@@ -480,6 +480,12 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
+  /*Configure GPIO pin : PB7 */
+  GPIO_InitStruct.Pin = GPIO_PIN_7;
+  GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
   /* USER CODE BEGIN MX_GPIO_Init_2 */
 
   /* USER CODE END MX_GPIO_Init_2 */
